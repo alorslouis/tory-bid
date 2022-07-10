81053b99c79e6ddecef7bc0f43f729e466710e9e
alorslouis <alorslouis@users.noreply.github.com>
2022-07-09 17:17:51 -0700

    +: gFetch, +: tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,57 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import styles from "../styles/Home.module.css";
+
+const dummyData = [{ id: 1, name: "rishi" }, { id: 2 }, { id: 3 }];
+
+// type params for getGSheetsData
+// const params = {
+//   spreadsheetId = "1ffqemZ-YOi7AvAw8HbxmMd0vIbsOXLZ7KpAmNQPD2r8";
+// }
+
+const requestOptions = {
+  method: "GET",
+  redirect: "follow",
+  key: process.env.KEY,
+};
+
+function getGSheetsData(params: type) {
+  return fetch(
+    `https://sheets.googleapis.com/v4/spreadsheets/${params.spreadsheetId}/values:batchGet?ranges=${params.range}&majorDimension=ROWS&key=${params.apiKey}`
+  )
+    .then((res) => res.json())
+    .then((data) => data.valueRanges[0].values);
+}
+
+async function getServerSideProps(params: string) {
+  const res = await fetch(
+    `https://sheets.googleapis.com/v4/spreadsheets/1ffqemZ-YOi7AvAw8HbxmMd0vIbsOXLZ7KpAmNQPD2r8/values/MP Round!A3:K22?majorDimension=COLUMNS&valueRenderOption=FORMULA`
+  );
+  const data = await res.json();
+
+  // Pass data to the page via props
+  return { props: { data } };
+}
 
 const Home: NextPage = () => {
+  const [leaderData, setLeaderData] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(
+      `https://sheets.googleapis.com/v4/spreadsheets/1ffqemZ-YOi7AvAw8HbxmMd0vIbsOXLZ7KpAmNQPD2r8/values/MP Round!A3:K22?majorDimension=COLUMNS&valueRenderOption=FORMULA&key=${process.env.NEXT_PUBLIC_GKEY}`,
+      requestOptions
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setLeaderData(data.values);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,44 +61,26 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className="text-8xl font-extrabold py-3">toryBids</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
+        {/* <p>{dummyData[0].id}</p> */}
+        <ul className="grid grid-cols-2 gap-4">
+          {dummyData.map((item) => (
+            <li key={item.id}>
+              {item.id},{item?.name}
+            </li>
+          ))}
+        </ul>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div>
+          {leaderData?.map((item) => (
+            <li key={item.id} className="list-none">
+              <p className="font-extrabold">{item[0]}</p>
+            </li>
+          ))}
         </div>
+        {/* <p>{leaderData.values[0]}</p> */}
+        {/* <p>{leaderData}</p> */}
       </main>
 
       <footer className={styles.footer}>
@@ -59,14 +89,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
