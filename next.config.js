cb89184d2f92eed127947cdcc1e984166943f3c1
alorslouis <alorslouis@users.noreply.github.com>
2022-07-11 13:58:55 -0700

    +: fetch thumbnail
    +: remote image domain to next.config.js
    
    +: Image component to profile, fetch via thumbnail url (originally portrait but was failing for some due to missing link)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["members-api.parliament.uk"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/profile/[name].tsx
+++ pages/profile/[name].tsx
@@ -2,6 +2,7 @@ import { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { GetServerSideProps } from "next";
+import Image from "next/image";
 
 const Profile: NextPage = ({ data1, data2 }) => {
   const router = useRouter();
@@ -13,6 +14,9 @@ const Profile: NextPage = ({ data1, data2 }) => {
         <div>passed: {name}</div>
         get: {data1?.items[0].value.id}, {data1.items[0].value.nameDisplayAs}
         get2: {data2?.value}
+        <div>
+          <Image src={data2?.value} width={300} height={300} />
+        </div>
       </div>
     </>
   );
@@ -26,7 +30,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const data1 = await res1.json();
 
   const res2 = await fetch(
-    `https://members-api.parliament.uk/api/Members/${data1?.items[0].value.id}/PortraitUrl`
+    `https://members-api.parliament.uk/api/Members/${data1?.items[0].value.id}/thumbnailUrl`
   );
   const data2 = await res2.json();
 
