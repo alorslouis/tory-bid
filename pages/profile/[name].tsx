725aab4db6f4c2d663d0edf1fbc9cb37948f423b
alorslouis <alorslouis@users.noreply.github.com>
2022-07-13 19:22:28 -0700

    +: error handling on failed fetch
    +: *finally* got error handling working for failed routes :scream:

--- pages/profile/[name].tsx
+++ pages/profile/[name].tsx
@@ -68,32 +68,34 @@ const Profile: NextPage = ({ data1, data2 }) => {
                     ))
                   : "none"}
               </div> */}
-              <div className="text-center flex-1 p-4">
-                <span className="text-lg font-extrabold">government posts</span>
-                <hr />
-                <table className="border-collapse table-auto w-full text-sm">
-                  <thead>
-                    <tr>
-                      <th>Post</th>
-                      <th>From</th>
-                      <th>Until </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {data2.value.governmentPosts[0]
-                      ? data2.value.governmentPosts.map((e) => (
-                          <tr>
-                            <td className="text-start">{e.name}</td>
-                            <td className="">{e.startDate}</td>
-                            <td className="">
-                              {e?.endDate ? e?.endDate : "now"}
-                            </td>
-                          </tr>
-                        ))
-                      : "none"}
-                  </tbody>
-                </table>
-              </div>
+              {data2.value.governmentPosts[0] ? (
+                <div className="text-center flex-1 p-4">
+                  <span className="text-lg font-extrabold">
+                    government posts
+                  </span>
+                  <hr />
+                  <table className="border-collapse table-auto w-full text-sm">
+                    <thead>
+                      <tr>
+                        <th>Post</th>
+                        <th>From</th>
+                        <th>Until </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {data2.value.governmentPosts.map((e) => (
+                        <tr>
+                          <td className="text-start">{e.name}</td>
+                          <td className="">{e.startDate}</td>
+                          <td className="">
+                            {e?.endDate ? e?.endDate : "now"}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              ) : null}
               <div className="text-center p-4">
                 <span className="text-lg font-extrabold">
                   committee memberships
@@ -134,18 +136,27 @@ const Profile: NextPage = ({ data1, data2 }) => {
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   // Fetch data from external API
-  const res1 = await fetch(
-    `https://members-api.parliament.uk/api/Members/Search?Name=${context.params.name}`
-  );
-  const data1 = await res1?.json();
+  try {
+    const res1 = await fetch(
+      `https://members-api.parliament.uk/api/Members/Search?Name=${context.params.name}`
+    );
+    const data1 = await res1?.json();
 
-  const res2 = await fetch(
-    `https://members-api.parliament.uk/api/Members/${data1?.items[0]?.value?.id}/Biography`
-  );
-  const data2 = await res2?.json();
+    const res2 = await fetch(
+      `https://members-api.parliament.uk/api/Members/${data1?.items[0]?.value?.id}/Biography`
+    );
+    const data2 = await res2?.json();
 
-  // Pass data to the page via props
-  return { props: { data1, data2 } };
+    // Pass data to the page via props
+    return { props: { data1, data2 } };
+  } catch (error) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
 };
 
 export default Profile;
