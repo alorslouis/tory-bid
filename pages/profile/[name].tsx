8381dd750546524306e1250031ad90c212806207
alorslouis <alorslouis@users.noreply.github.com>
2022-07-17 14:31:25 -0700

    +: [profile/[name]] - swr fetch of bio

--- pages/profile/[name].tsx
+++ pages/profile/[name].tsx
@@ -5,9 +5,9 @@ import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import Layout from "../../components/layout";
-3;
 
 import { z } from "zod";
+import useSWR from "swr";
 
 function parseDate(date: string) {
   const forDate = new Date(Date.parse(date));
@@ -20,7 +20,7 @@ function parseDate(date: string) {
 }
 
 type RootSchema = {
-  data: {
+  data1: {
     items: Array<{
       value: {
         id: number;
@@ -77,6 +77,74 @@ type RootSchema = {
   };
 };
 
+export type BioSchema = {
+  data2: {
+    value: {
+      representations: Array<{
+        house: number;
+        name: string;
+        id: number;
+        startDate: string;
+        endDate: any;
+        additionalInfo: string;
+        additionalInfoLink: any;
+      }>;
+      electionsContested: Array<{
+        house: number;
+        name: string;
+        id: number;
+        startDate: string;
+        endDate: any;
+        additionalInfo: any;
+        additionalInfoLink: any;
+      }>;
+      houseMemberships: Array<{
+        house: number;
+        name: string;
+        id: number;
+        startDate: string;
+        endDate: any;
+        additionalInfo: string;
+        additionalInfoLink: any;
+      }>;
+      governmentPosts: Array<{
+        house: number;
+        name: string;
+        id: number;
+        startDate: string;
+        endDate?: string;
+        additionalInfo: string;
+        additionalInfoLink: string;
+      }>;
+      oppositionPosts: Array<any>;
+      otherPosts: Array<any>;
+      partyAffiliations: Array<{
+        house: number;
+        name: string;
+        id: number;
+        startDate: string;
+        endDate: any;
+        additionalInfo: any;
+        additionalInfoLink: any;
+      }>;
+      committeeMemberships: Array<{
+        house: number;
+        name: string;
+        id: number;
+        startDate: string;
+        endDate: string;
+        additionalInfo: any;
+        additionalInfoLink: any;
+      }>;
+    };
+    links: Array<{
+      rel: string;
+      href: string;
+      method: string;
+    }>;
+  };
+};
+
 // export const getServerSideProps: GetServerSideProps = async (context) => {
 //   // try {
 //     const res1 = await fetch(
@@ -105,10 +173,10 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     const res = await fetch(
       `https://members-api.parliament.uk/api/Members/Search?Name=${context?.params?.name}`
     );
-    const data = (await res.json()) as RootSchema;
-    console.log(data);
+    const data1 = (await res.json()) as RootSchema;
+    // console.log(data1);
 
-    return { props: { data } };
+    return { props: { data1 } };
   } catch (error) {
     return {
       notFound: true,
@@ -120,10 +188,20 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 };
 
 function Profile({
-  data,
+  data1,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router = useRouter();
   const { name } = router.query;
+
+  const fetcher = (url: string) => fetch(url).then((r) => r.json());
+
+  const { data, error } = useSWR(
+    `https://members-api.parliament.uk/api/Members/${data1?.items[0]?.value?.id}/Biography`,
+    fetcher
+  );
+
+  const bioData: BioSchema = data;
+  console.log(bioData);
 
   // const member = { data1 };
   // console.log(member);
@@ -131,7 +209,7 @@ function Profile({
   //   Date.parse(data1.value.latestHouseMembership.membershipStartDate)
   // );
 
-  if (!data?.items[0]?.value?.id) {
+  if (!data1?.items[0]?.value?.id) {
     return (
       <>
         <Layout>
@@ -148,61 +226,53 @@ function Profile({
     );
   }
 
-  {
-    useEffect(() => {
-      setLoading(true);
-      fetch("/api/profile-data")
-        .then((res) => res.json())
-        .then((data) => {
-          setData(data);
-          setLoading(false);
-        });
-    }, []);
+  // if (error) return <div>failed to load</div>;
+  // if (!data) return <div>loading...</div>;
 
-    if (isLoading) return <p>Loading...</p>;
-    if (!data) return <p>No profile data</p>;
+  {
     return (
       <>
         <Layout>
-          {/* <div className="lg:flex-col">{data1.items.value}</div> */}
+          {/* <div className="lg:flex-col">{data11.items.value}</div> */}
           <div className="lg:flex-col">{name}</div>
-          <div className="lg:flex-col">{data?.items[0]?.value?.id}</div>
-          <div className="lg:flex-col">{data?.items[0]?.value?.nameListAs}</div>
+          <div className="lg:flex-col">{data1?.items[0]?.value?.id}</div>
+          <div className="lg:flex-col">
+            {data1?.items[0]?.value?.nameListAs}
+          </div>
           <div className=" m-4 bg-blue-400 rounded-xl text-zinc-200 ">
             <div className="lg:flex-col">
               <div className="rounded-full flex h-full pb-4">
                 <span className="cursor-pointer">
                   <Link href="/">
                     <Image
-                      src={data.items[0].value.thumbnailUrl}
+                      src={data1.items[0].value.thumbnailUrl}
                       width={300}
                       height={300}
                       objectFit="fill"
                     />
                   </Link>
                 </span>
+
                 <div className="p-4">
                   <h1 className="text-4xl font-semibold">{name}</h1>
                   <p>
                     MP for{" "}
-                    {data.items[0].value.latestHouseMembership.membershipFrom}
+                    {data1.items[0].value.latestHouseMembership.membershipFrom}
                     {/* {
                     data1?.items[0].value.latestHouseMembership
                       .membershipStartDate
                   } */}
                   </p>
                   {/* Member since {memberDate.getFullYear()} */}
-                  <p></p>
+                  {/* <p>{data.value[0].type}</p> */}
                   {/* <p>{data2?.value.representations[0].additionalInfo}</p> */}
+                  <p>{data?.value.representations[0].additionalInfo}</p>
                 </div>
-              </div>{" "}
-              <hr className="bg-white" />
+                <hr className="bg-white" />
+                {/* <div>{bioData.value.representations[0].id}</div> */}
+              </div>
             </div>
           </div>
-          {/* {data.items} */}
-          {/* <div className="lg:flex-col">{data1.values}</div> */}
-          {/* {data1.items.value} */}
-          {/* <div className="lg:flex-col">{data1.value.id}</div> */}
         </Layout>
       </>
     );
